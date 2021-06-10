fab33857b0eae416aabedb24976ff9fa8e2205d4
ryunosuke35 <6bev2103@gmail.com>
2021-06-11 00:59:59 +0900

    システムスペックのテストの実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,7 +11,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to tasks_path, notice: "登録が完了しました！"
+      redirect_to @task, notice: "登録が完了しました！"
     else
       render :new, status: :unprocessable_entity
     end
