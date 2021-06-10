e86e4b56fa48c49537d2409508976f1980439a10
ryunosuke35 <6bev2103@gmail.com>
2021-06-10 15:08:09 +0900

    タスクの閲覧・登録・更新・削除機能の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,25 +1,52 @@
 class TasksController < ApplicationController
 
   def index
+    @task = Task.all
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "登録が完了しました！"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "編集が完了しました！"
+    else
+      render :edit
+    end
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
-  def confirm
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, notice: "削除しました！"
   end
 
+
+
+
+
+  private
+  def task_params
+    @task = params.require(:task).permit(:name, :content)
+  end
 
 end
