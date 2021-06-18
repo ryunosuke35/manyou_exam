835b60fbbeef827d606a1baf4596926ab9fd3e96
ryunosuke35 <6bev2103@gmail.com>
2021-06-18 17:59:22 +0900

    seedデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'matsumura', email: 'seed@example.com', password_digest: 'asdf123')
