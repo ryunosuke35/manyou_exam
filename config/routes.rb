de6654c5eaf517895b90334fe6657844a2e1886f
ryunosuke35 <6bev2103@gmail.com>
2021-06-19 10:12:38 +0900

    ログインができるように設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'tasks#index'
   resources :tasks
   resources :users
+  resources :sessions
 end
