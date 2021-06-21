2fbd07fb8c5d661bedaec2dc66f08ffc29b6acb5
ryunosuke35 <6bev2103@gmail.com>
2021-06-21 23:36:53 +0900

    taskとlabelの多対多のアソシエーションを作成

--- app/models/task.rb
+++ app/models/task.rb
@@ -13,4 +13,7 @@ class Task < ApplicationRecord
   enum priority: { 高: 0, 中: 1, 低: 2 }
 
   belongs_to :user
+  has_many :labellings, dependent: :destroy
+  has_many :labels, through: :labellings
+
 end
