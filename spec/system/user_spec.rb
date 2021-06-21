eb72d9bb63992cb45f5bce200b3188dda315ba5f
ryunosuke35 <6bev2103@gmail.com>
2021-06-21 15:56:09 +0900

    モデルのtask.specのバグを修正

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -6,6 +6,21 @@ describe 'ユーザ登録・セッション機能・管理画面のテスト', t
     FactoryBot.create(:second_user)
   end
 
+  def admin_login
+    visit new_session_path
+    fill_in 'session_email', with: 'default@gmail.com'
+    fill_in 'session_password', with: 'asdf123'
+    click_on 'ログイン'
+  end
+
+  def general_login
+    visit new_session_path
+    fill_in 'session_email', with: 'default2@gmail.com'
+    fill_in 'session_password', with: 'asdf123'
+    click_on 'ログイン'
+  end
+
+
   describe 'ユーザ登録のテスト' do
     context 'ユーザを新規登録した場合' do
       it '作成したタスク・終了期限・ステータスが表示される' do
@@ -31,42 +46,27 @@ describe 'ユーザ登録・セッション機能・管理画面のテスト', t
   describe 'セッション機能のテスト' do
     context 'ログイン操作をした場合' do
       it 'ログインできる' do
-        visit new_session_path
-        fill_in 'session_email', with: 'default@gmail.com'
-        fill_in 'session_password', with: 'asdf123'
-        click_on 'ログイン'
+        admin_login
       end
     end
     context 'ログインした場合' do
       it '自分の詳細画面(マイページ)にとぶ' do
-        visit new_session_path
-        fill_in 'session_email', with: 'default@gmail.com'
-        fill_in 'session_password', with: 'asdf123'
-        click_on 'ログイン'
+        admin_login
         expect(page).to have_content 'デフォルトの名前'
         expect(page).to have_content 'efault@gmail.com'
       end
     end
     context '一般ユーザが他人の詳細画面にとんだ場合' do
-      it 'タスク一覧画面に遷移する' do
-        visit new_session_path
-        fill_in 'session_email', with: 'default@gmail.com'
-        fill_in 'session_password', with: 'asdf123'
-        click_on 'ログイン'
-        user_id = User.ids[1]
-        visit user_path(user_id)
-        expect(page).to have_content '一覧'
+      it '自分のタスク一覧画面に遷移する' do
+        general_login
         user_id = User.ids[0]
         visit user_path(user_id)
-        expect(page).not_to have_content '一覧'
+        expect(page).to have_content '一覧'
       end
     end
     context 'ログアウトした場合' do
       it '自分の詳細画面(マイページ)にとぶ' do
-        visit new_session_path
-        fill_in 'session_email', with: 'default@gmail.com'
-        fill_in 'session_password', with: 'asdf123'
-        click_on 'ログイン'
+        admin_login
         click_link 'Logout'
         expect(page).to have_content 'ログアウトしました'
       end
@@ -77,20 +77,14 @@ describe 'ユーザ登録・セッション機能・管理画面のテスト', t
   describe '管理画面のテスト' do
     context '管理ユーザが管理画面にアクセスした場合' do
       it '管理画面のユーザー一覧画面へとぶ' do
-        visit new_session_path
-        fill_in 'session_email', with: 'default@gmail.com'
-        fill_in 'session_password', with: 'asdf123'
-        click_on 'ログイン'
+        admin_login
         visit admin_users_path
         expect(page).to have_content '管理画面のユーザー一覧'
       end
     end
     context '一般ユーザが管理画面にアクセスした場合' do
       it 'タスク一覧画面へとぶ' do
-        visit new_session_path
-        fill_in 'session_email', with: 'default2@gmail.com'
-        fill_in 'session_password', with: 'asdf123'
-        click_on 'ログイン'
+        general_login
         visit admin_users_path
         expect(page).to have_content '管理者権限がありません！'
         expect(page).to have_content '一覧'
@@ -98,10 +92,7 @@ describe 'ユーザ登録・セッション機能・管理画面のテスト', t
     end
     context '管理ユーザがユーザの新規登録をした場合' do
       it '一覧画面にとび、登録者の名前とメールアドレスが表示される' do
-        visit new_session_path
-        fill_in 'session_email', with: 'default@gmail.com'
-        fill_in 'session_password', with: 'asdf123'
-        click_on 'ログイン'
+        admin_login
         visit admin_users_path
         click_link '新規登録'
         fill_in 'user_name', with: '松村龍之介'
@@ -117,10 +108,7 @@ describe 'ユーザ登録・セッション機能・管理画面のテスト', t
     end
     context '管理ユーザがユーザの詳細画面したにアクセスした場合' do
       it 'ユーザの情報と、そのユーザのタスク一覧が表示される' do
-        visit new_session_path
-        fill_in 'session_email', with: 'default@gmail.com'
-        fill_in 'session_password', with: 'asdf123'
-        click_on 'ログイン'
+        admin_login
         visit admin_users_path
         all('tbody tr')[1].click_link '詳細'
         expect(page).to have_content 'デフォルトの名前2'
@@ -134,10 +122,7 @@ describe 'ユーザ登録・セッション機能・管理画面のテスト', t
     end
     context '管理ユーザがユーザの情報を編集するリンクをクリックした場合' do
       it '管理者一覧画面にとび、情報が編集される' do
-        visit new_session_path
-        fill_in 'session_email', with: 'default@gmail.com'
-        fill_in 'session_password', with: 'asdf123'
-        click_on 'ログイン'
+        admin_login
         visit admin_users_path
         all('tbody tr')[1].click_link '編集'
         fill_in 'user_name', with: 'デフォルトの名前3'
@@ -152,10 +137,7 @@ describe 'ユーザ登録・セッション機能・管理画面のテスト', t
     end
     context '管理ユーザがユーザの情報を削除するリンクをクリックした場合' do
       it '管理者一覧画面にとび、情報が削除される' do
-        visit new_session_path
-        fill_in 'session_email', with: 'default@gmail.com'
-        fill_in 'session_password', with: 'asdf123'
-        click_on 'ログイン'
+        admin_login
         visit admin_users_path
         all('tbody tr')[1].click_link '削除'
         page.driver.browser.switch_to.alert.accept
